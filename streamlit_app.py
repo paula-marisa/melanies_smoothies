429c541e9f9cc1daf9e78b4fbdd39f52ddf57d15
Paula Rodrigues <paulamsr@hotmail.com>
2025-07-16 14:15:19 +0100

    Update streamlit_app.py
    Add dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_with=True)
